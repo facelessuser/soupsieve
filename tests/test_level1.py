84299e5fdb6fad4d641e0242364fd013fd681cd4
facelessuser <faceless.shop@gmail.com>
2018-12-11 21:39:15 -0600

    Complete css_parser unittest

--- tests/test_level1.py
+++ tests/test_level1.py
@@ -139,3 +139,21 @@ class TestLevel1(util.TestCase):
             ["4"],
             mode=sv.HTML5
         )
+
+    def test_escapes(self):
+        """Test escapes."""
+
+        markup = """
+        <div>
+        <p>Some text <span id="1" class="foo:bar:foobar"> in a paragraph</span>.
+        <a id="2" class="bar" href="http://google.com">Link</a>
+        </p>
+        </div>
+        """
+
+        self.assert_selector(
+            markup,
+            ".foo\\:bar\\3a foobar",
+            ["1"],
+            mode=sv.HTML5
+        )
