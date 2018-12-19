025e80d1f62e17a9baab8ecd780bb4139f9fd6bc
facelessuser <faceless.shop@gmail.com>
2018-12-19 12:13:09 -0700

    Prep for release

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
