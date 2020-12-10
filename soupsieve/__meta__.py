826627ccad5e29088352135e41baf75014325e51
Isaac Muse <faceless.shop@gmail.com>
2020-12-10 10:52:11 -0700

    Drop Python 3.5 (#208)

--- soupsieve/__meta__.py
+++ soupsieve/__meta__.py
@@ -185,5 +185,5 @@ def parse_version(ver, pre=False):
     return Version(major, minor, micro, release, pre, post, dev)
 
 
-__version_info__ = Version(2, 0, 2, ".dev")
+__version_info__ = Version(2, 1, 0, "final")
 __version__ = __version_info__._get_canonical()
