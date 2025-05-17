65df5296cf2a60d49feb2e312cb4354cb690050a
facelessuser <faceless.shop@gmail.com>
2025-05-17 10:28:01 -0600

    Add support for Python 3.14 and drop Python 3.8

--- hatch_build.py
+++ hatch_build.py
@@ -29,12 +29,12 @@ class CustomMetadataHook(MetadataHookInterface):
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.12',
             'Programming Language :: Python :: 3.13',
+            'Programming Language :: Python :: 3.14',
             'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Typing :: Typed'
